ad4b20e79953c843eb181075b15942516c8c569b
Cheul Young Park <cheulyop@gmail.com>
2020-11-08 14:21:29 +0000

    Moved transform_label function to utils

--- utils.py
+++ utils.py
@@ -8,3 +8,12 @@ def get_config(path):
         config = json.loads(f.read(), object_hook=lambda d: Namespace(**d))
 
     return config
+
+
+def transform_label(target, pos_label):
+
+    def transform_fn(a, v):
+        label = a if target == 'arousal' else v
+        return int(label > 2) if pos_label == 'high' else int(label <= 2)
+
+    return transform_fn
