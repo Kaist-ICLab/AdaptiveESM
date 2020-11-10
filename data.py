e0b99314478418a316d2691a820509554205ddfb
Cheul Young Park <cheulyop@gmail.com>
2020-11-10 13:24:52 +0000

    Add concatenating segments via rolling method, feature extraction with PyTEAP, and classifiation with XGBoost

--- data.py
+++ data.py
@@ -1,10 +1,14 @@
 import os
 import json
+import warnings
 import numpy as np
+from tqdm import tqdm
 
-from collections import OrderedDict, Counter
-from scipy.interpolate import interp1d
 from scipy.signal import decimate
+from scipy.interpolate import interp1d
+from collections import OrderedDict, Counter
+from sklearn.preprocessing import StandardScaler
+from pyteap.signals import bvp, gsr, hst, ecg
 
 import torch
 import pytorch_lightning as pl
@@ -18,29 +22,49 @@ class KEMOCONDataModule(pl.LightningDataModule):
         assert config.label_type in {'self', 'partner', 'external'}, f'label_type must be one of "self", "partner", and "external", but given "{config.label_type}".'
         # assert fusion is None or fusion in {'stack', 'decision', 'autoencoder'}, f'fusion must be one of "feature", "decision", and "autoencoder", but given "{fusion}".'
 
-        self.data_dir       = os.path.expanduser(config.data_dir)
-        self.batch_size     = config.batch_size
-        self.label_type     = config.label_type
-        self.n_classes      = config.n_classes
-        self.val_size       = config.val_size
+        self.sigtypes           = ['bvp', 'eda', 'temp', 'ecg']
+        self.sample_rates       = [64, 4, 4, 1]
 
-        self.resample       = config.resample
-        self.standardize    = config.standardize
-        self.fusion         = config.fusion
-        self.label_fn       = label_fn
-        self.ids            = self.get_ids()
+        self.data_dir           = os.path.expanduser(config.data_dir)
+        self.batch_size         = config.batch_size
+        self.label_type         = config.label_type
+        self.n_classes          = config.n_classes
+        self.val_size           = config.val_size
+        self.num_segs           = config.num_segs  # number of segments in one input (minimum 5)
 
-    def prepare_data(self):
+        self.resample           = config.resample
+        self.extract_features   = config.extract_features
+        self.standardize        = config.standardize
+        self.fusion             = config.fusion
+        self.label_fn           = label_fn
+        self.ids                = self.get_ids()
+
+        if self.resample and self.extract_features:
+            warnings.warn('Resampling and feature extraction are mutually exclusive (cannot extract features from downsampled BVP signals), extract_features is set to false.', UserWarning)
+            self.extract_features = False
+
+    def get_features(self, sig, sr, sigtype):
+        if sigtype == 'bvp':
+            features = bvp.get_bvp_features(bvp.acquire_bvp(sig, sr), sr)
+        elif sigtype == 'eda':
+            features = gsr.get_gsr_features(gsr.acquire_gsr(sig, sr, conversion=1e6), sr)
+        elif sigtype == 'temp':
+            features = hst.get_hst_features(hst.acquire_hst(sig, sr), sr)
+        elif sigtype == 'ecg':
+            features = ecg.get_ecg_features(sig)
+        return features
+
+    def prepare_data(self, check_id=False):
         # Note: prepare_data is called from a single GPU. Do not use it to assign state (self.x = y)
         # load data from data_dir
-        pid_to_segments = {}
+        pid_to_segments = dict()
 
         # for each participant
-        for pid in map(int, os.listdir(self.data_dir)):
-            pid_to_segments.setdefault(pid, [])
-            curr_x = pid_to_segments[pid]
+        for pid in sorted(map(int, os.listdir(self.data_dir))):
+            pid_to_segments.setdefault(pid, list())
+            segments = pid_to_segments[pid]
 
-            # for segments for a participant
+            # load segments
             pdir = os.path.join(self.data_dir, str(pid))
             for segname in os.listdir(pdir):
                 # get segment index
@@ -60,55 +84,92 @@ class KEMOCONDataModule(pl.LightningDataModule):
                 else:
                     label = (a, v)
 
-                # get signals
+                # get signals (5s long)
                 with open(os.path.join(pdir, segname)) as f:
                     seg = json.load(f)
                     # adjust signal lengths (cut or pad right edge)
-                    for sigtype, sr in [('bvp', 64), ('eda', 4), ('temp', 4), ('ecg', 1)]:
-                        curr_sig = seg[sigtype]
-                        seg[sigtype] = curr_sig[:sr * 5] if len(curr_sig) > sr * 5 else np.pad(curr_sig, pad_width=(0, sr * 5 - len(curr_sig)), mode='edge')
-                    curr_x.append([idx, seg, label])
+                    for sigtype, sr in zip(self.sigtypes, self.sample_rates):
+                        sig = seg[sigtype]
+                        seg[sigtype] = sig[:sr * 5] if len(sig) > sr * 5 else np.pad(sig, pad_width=(0, sr * 5 - len(sig)), mode='edge')
+                    # append current segment to list
+                    segments.append([idx, seg, label])
+
+            # sort list of segments by index
+            segments.sort(key=lambda x: x[0])
 
             # check label distribution: if the number of unique classes for the current participant does not equal n_classes,
-            # we will remove current participant from the dataset as such participant's data cannot be used as a test set 
-            if len(Counter(map(lambda x: x[-1], curr_x))) != self.n_classes:
+            # remove current participant from the dataset as such participant's data cannot be used for testing
+            if len(Counter(map(lambda x: x[-1], segments))) != self.n_classes:
                 del pid_to_segments[pid]
                 continue
-                
-            # apply up/downsampling
+            
+            # break loop if only checking for participant ids in the data
+            if check_id:
+                continue
+            
+            # concat N = num_segs segments (each 5s) via a rolling method
+            curr_x = list()
+            for i in range(len(segments) - self.num_segs + 1):
+                segs = segments[i:i + self.num_segs]  # segments to be concatenated
+                # concat segments for each signal type
+                seg = {sigtype: np.concatenate([seg[sigtype] for _, seg, _ in segs]) for sigtype in self.sigtypes}
+                curr_x.append([i, seg, segs[-1][-1]])  # take the label of the last segment
+
+            # apply up/downsampling - upsample ECG (=heart rate) and downsample bvp
             if self.resample:
                 # for each segment
-                for _, seg, _ in curr_x:
+                for i, (_, seg, _) in enumerate(curr_x):
                     # upsample ecg signals from 1hz to 4hz
-                    x = np.linspace(1, 5, num=5, endpoint=True)
-                    x_new = np.linspace(1, 5, num=20, endpoint=True)
+                    x = np.linspace(1, (self.num_segs * 5), num=(self.num_segs * 5), endpoint=True)
+                    x_new = np.linspace(1, (self.num_segs * 5), num=(self.num_segs * 5 * 4), endpoint=True)
                     seg['ecg'] = interp1d(x, seg['ecg'], kind='quadratic')(x_new)
                     # downsample bvp signals from 64hz to 4hz
                     seg['bvp'] = decimate(seg['bvp'], 16, zero_phase=True)
 
-            # apply standardization
-            if self.standardize:
+            # extract features: resampling and feature extraction are mutually exclusive (can't extract features from resampled BVP signals)
+            if not self.resample and self.extract_features:
+                idx_to_del = list()  # list to store indices that will be deleted
+                for i, (idx, seg, label) in tqdm(enumerate(curr_x), desc=f'Participant {pid:02d}', ascii=True, dynamic_ncols=True):
+                    features = np.concatenate([self.get_features(seg[sigtype], sr, sigtype) for sigtype, sr in zip(self.sigtypes, self.sample_rates)])
+                    # replace raw signals with features
+                    curr_x[i] = [idx, features, label]
+                    # current item will be removed if there is any feature that is nan
+                    if np.isnan(features).any():
+                        idx_to_del.append(i)
+                # delete items with index in idx_to_del
+                if idx_to_del:
+                    curr_x = np.delete(curr_x, idx_to_del, axis=0).tolist()
+
+            # apply signal-wise standardization (to raw signals)
+            if self.standardize and not self.extract_features:
                 # for each signal type
-                for sigtype in ['bvp', 'eda', 'temp', 'ecg']:
-                    # get all signal values of type to compute mean and std
+                for sigtype in self.sigtypes:
+                    # concat all signals of type along time axis to compute mean and std
                     sig = np.concatenate(list(map(lambda x: x[1][sigtype], curr_x)))
                     # standardize each segment with mean and std
                     for _, seg, _ in curr_x:
                         seg[sigtype] = (seg[sigtype] - np.mean(sig)) / np.std(sig)
 
-            # apply feature fusion by stacking
-            if self.resample and self.fusion == 'stack':
-                for i, (_, seg, _) in enumerate(curr_x):
-                    curr_x[i][1] = torch.Tensor([seg['bvp'], seg['eda'], seg['temp'], seg['ecg']]).T
+            # apply feature-wise standardization (to extracted features)
+            elif self.standardize and self.extract_features:
+                scaler = StandardScaler()
+                scaler.fit(np.stack([features for _, features, _ in curr_x]))  # fit scaler to features
+                for i, (_, features, _) in enumerate(curr_x):
+                    curr_x[i][1] = np.squeeze(scaler.transform(features.reshape(1, -1)))  # standardize features with scaler
 
-            # sort segments for current participant by segment indices
-            curr_x.sort(key=lambda x: x[0])
+            # apply fusion by stacking (only to raw signals)
+            if self.resample and not self.extract_features and self.fusion == 'stack':
+                for i, (_, seg, _) in enumerate(curr_x):
+                    curr_x[i][1] = np.transpose([seg[sigtype] for sigtype in self.sigtypes])
+
+            # save processed segments to dict
+            pid_to_segments[pid] = curr_x
 
         # return dict sorted by pid
         return OrderedDict(sorted(pid_to_segments.items(), key=lambda x: x[0]))
 
     def get_ids(self):
-        return self.prepare_data().keys()
+        return self.prepare_data(check_id=True).keys()
 
     def setup(self, stage=None, test_id=None):
         # setup expects a string arg stage. It is used to separate setup logic for trainer.fit and trainer.test.
@@ -118,18 +179,18 @@ class KEMOCONDataModule(pl.LightningDataModule):
         # for loso cross-validation
         if test_id is not None:
             if stage == 'test' or stage is None:
-                inp, tgt = zip(*[(seg, label) for _, seg, label in data[test_id]])
+                inp, tgt = zip(*[(torch.Tensor(seg), label) for _, seg, label in data[test_id]])
                 self.kemocon_test = TensorDataset(torch.stack(inp), torch.Tensor(tgt).unsqueeze(1))
                 self.dims = tuple(self.kemocon_test[0][0].shape)
 
             if stage == 'fit' or stage is None:
-                inp, tgt = zip(*[(seg, label) for pid in data if pid != test_id for _, seg, label in data[pid]])
+                inp, tgt = zip(*[(torch.Tensor(seg), label) for pid in data if pid != test_id for _, seg, label in data[pid]])
                 kemocon_full = TensorDataset(torch.stack(inp), torch.Tensor(tgt).unsqueeze(1))
                 n_val = int(self.val_size * len(kemocon_full))
                 self.kemocon_train, self.kemocon_val = random_split(
                     dataset     = kemocon_full,
                     lengths     = [len(kemocon_full) - n_val, n_val],
-                    generator   = torch.Generator(),
+                    # generator   = torch.Generator(),
                 )
                 self.dims = tuple(self.kemocon_train[0][0].shape)
         
@@ -137,13 +198,13 @@ class KEMOCONDataModule(pl.LightningDataModule):
         # given val_size which is a float between 0 and 1 defining the proportion of validation set
         # validation and test sets will have the same size of val_size * full dataset, and train set will be the rest of the data
         else:
-            inp, tgt = zip(*[(seg, label) for pid in data for _, seg, label in data[pid]])
+            inp, tgt = zip(*[(torch.Tensor(seg), label) for pid in data for _, seg, label in data[pid]])
             kemocon_full = TensorDataset(torch.stack(inp), torch.Tensor(tgt).unsqueeze(1))
             n_val = int(self.val_size * len(kemocon_full))
             train, valid, test = random_split(
                 dataset     = kemocon_full,
                 lengths     = [len(kemocon_full) - (n_val * 2), n_val, n_val],
-                generator   = torch.Generator(),
+                # generator   = torch.Generator(),
             )
 
             if stage == 'fit' or stage is None:
--- experiment.py
+++ experiment.py
@@ -1,5 +1,6 @@
 # import system 
 import os
+import time
 import json
 import argparse
 import pandas as pd
@@ -9,9 +10,13 @@ from argparse import Namespace
 # import custom modules
 from data import KEMOCONDataModule
 from utils import get_config, transform_label
-from models import LSTM, StackedLSTM, TransformerNet
+from models import LSTM, StackedLSTM, TransformerNet, XGBoost
 
-# import pytorch lightning related stuff
+# import pytorch related
+import torch
+from torch.utils.data import ConcatDataset
+
+# and pytorch-lightning related stuff
 import pytorch_lightning as pl
 from pytorch_lightning import seed_everything
 from pytorch_lightning.loggers import TensorBoardLogger, CometLogger
@@ -24,6 +29,7 @@ class Experiment(object):
     def __init__(self, config: str) -> None:
         # get configurations
         self.config = get_config(config)
+        self.config_dict = {key: vars(value) for key, value in vars(self.config).items()}
         
         # set seed
         seed_everything(self.config.exp.seed)
@@ -38,10 +44,12 @@ class Experiment(object):
         exp_name = f'{self.config.exp.model}_{self.config.exp.type}_{self.config.exp.target}_{self.config.exp.pos_label}'
         print(f'Experiment: {exp_name}, w/ participants: {list(self.dm.ids)}')
 
-        # make directory to save experiment results
-        self.savedir = os.path.expanduser(os.path.join(self.config.exp.savedir, exp_name))
-        os.makedirs(self.savedir, exist_ok=True)
+        # make directory to save results
+        os.makedirs(os.path.expanduser(self.config.exp.savedir), exist_ok=True)
 
+        # set path to save experiment results
+        self.savepath = os.path.expanduser(os.path.join(self.config.exp.savedir, f'{exp_name}_{int(time.time())}.json'))
+        
     def get_logger(self, pid):
         # set version number if needed
         version = "" if pid is None else f'_{pid:02d}'
@@ -65,85 +73,125 @@ class Experiment(object):
         return logger
 
     def _body(self, pid=None):
-        # get logger
-        self.logger = self.get_logger(pid)
+        # separate body for xgboost
+        if self.config.exp.model == 'xgboost':
+            self.model = XGBoost(self.config.hparams)
 
-        # init lr monitor and callbacks
-        self.callbacks = list()
-        if self.config.hparams.scheduler is not None:
-            self.callbacks.append(LearningRateMonitor(logging_interval='epoch'))
+            # setup datamodule
+            self.dm.setup(stage=None, test_id=pid)
 
-        # init early stopping
-        if self.config.early_stop is not None:
-            self.callbacks.append(EarlyStopping(**vars(self.config.early_stop)))
+            # train model: concat train and valid inputs and labels and convert torch tensors to numpy arrays
+            X_train, y_train = map(lambda x: torch.cat(x, dim=0).numpy(), zip(self.dm.kemocon_train[:], self.dm.kemocon_val[:]))
+            self.model.train(X_train, y_train)
 
-        # make model
-        if self.config.exp.model == 'lstm':
-            self.model = LSTM(self.config.hparams)
-        elif self.config.exp.model == 'stacked_lstm':
-            self.model = StackedLSTM(self.config.hparams)
-        elif self.config.exp.model == 'transformer':
-            self.model = TransformerNet(self.config.hparams)
+            # test model
+            X_test, y_test = map(lambda x: x.numpy(), self.dm.kemocon_test[:])
+            metr, cm = self.model.test(X_test, y_test)
+            
+        else:
+            # get logger
+            self.logger = self.get_logger(pid)
 
-        # make trainer
-        trainer_args = vars(self.config.trainer)
-        trainer_args.update({
-            'logger': self.logger,
-            'callbacks': self.callbacks,
-            'auto_lr_find': True if self.config.exp.tune else False
-        })
-        self.trainer = pl.Trainer(**trainer_args)
+            # init lr monitor and callbacks
+            self.callbacks = list()
+            if self.config.hparams.scheduler is not None:
+                self.callbacks.append(LearningRateMonitor(logging_interval='epoch'))
 
-        # find optimal lr
-        if self.config.exp.tune:
-            self.trainer.tune(self.model, datamodule=self.dm)
-        
-        # train model
-        self.dm.setup(stage='fit', test_id=pid)
-        self.trainer.fit(self.model, self.dm)
+            # init early stopping
+            if self.config.early_stop is not None:
+                self.callbacks.append(EarlyStopping(**vars(self.config.early_stop)))
 
-        # test model and get results
-        self.dm.setup(stage='test', test_id=pid)
-        results = self.trainer.test(self.model)[0]
+            # make model
+            if self.config.exp.model == 'lstm':
+                self.model = LSTM(self.config.hparams)
+            elif self.config.exp.model == 'stacked_lstm':
+                self.model = StackedLSTM(self.config.hparams)
+            elif self.config.exp.model == 'transformer':
+                self.model = TransformerNet(self.config.hparams)
 
-        # return metrics and confusion matrix
-        metrics = {
-            'pid': pid,
-            'acc': results['test_acc'],
-            'ap': results['test_ap'],
-            'f1': results['test_f1'],
-            'auroc': results['test_auroc'],
-            'num_epochs': self.model.current_epoch,
-        }
-        return metrics, self.model.test_confmat
+            # make trainer
+            trainer_args = vars(self.config.trainer)
+            trainer_args.update({
+                'logger': self.logger,
+                'callbacks': self.callbacks,
+                'auto_lr_find': True if self.config.exp.tune else False
+            })
+            self.trainer = pl.Trainer(**trainer_args)
+
+            # find optimal lr
+            if self.config.exp.tune:
+                self.trainer.tune(self.model, datamodule=self.dm)
+            
+            # setup datamodule
+            self.dm.setup(stage=None, test_id=pid)
+            print(self.dm.dims)
+
+            # train model
+            self.trainer.fit(self.model, self.dm)
+
+            # test model and get results
+            results = self.trainer.test(self.model)[0]
+
+            # return metrics and confusion matrix
+            metr = {
+                'pid': pid,
+                'acc': results['test_acc'],
+                'ap': results['test_ap'],
+                'f1': results['test_f1'],
+                'auroc': results['test_auroc'],
+                'num_epochs': self.model.current_epoch,
+            }
+            cm = self.model.test_confmat
+
+        return metr, cm
+
+    def _active_body(self, pid=None):
+        pass
 
     def run(self) -> None:
         # run k-fold cv
         if self.config.exp.type == 'kfold':
-            metrics, cm = self._body()
+            metr, cm = self._body()
+            results = {
+                'config': self.config_dict,
+                'metrics': metr,
+                'confmats': cm.tolist()
+            }
+            print(metr)
             print(cm)
 
-            # save results
-            with open(os.path.join(self.savedir, 'results.json'), 'w') as f:
-                json.dump({'metrics': metrics, 'confmat': cm.tolist()}, f, indent=4)
-
+        # run loso cv
         if self.config.exp.type == 'loso':
-            results, confmats = list(), dict()
+            metrics, confmats = list(), dict()
 
             # for each participant
             for pid in self.dm.ids:
                 # run loso cv and get results
-                metrics, cm = self._body(pid=pid)
-                results.append(metrics)
+                metr, cm = self._body(pid=pid)
+                metrics.append(metr)
                 confmats[pid] = cm.tolist()
-                print(cm)
+                print(f'pid: {pid},\n{cm}')
 
-            # save metrics as csv
-            pd.DataFrame(results).set_index('pid').to_csv(os.path.join(self.savedir, 'metrics.csv'))
+            # convert metrics for each participant to json string
+            metrics = pd.DataFrame(metrics).set_index('pid').to_json(orient='index')
 
-            # save confusion matrices
-            with open(os.path.join(self.savedir, 'confmat.json'), 'w') as f:
-                json.dump(confmats, f, sort_keys=True, inden=4)
+            # make results dict
+            results = {
+                'config': self.config_dict,
+                'metrics': metrics,
+                'confmats': confmats
+            }
+
+        # run active learning
+        # if self.config.exp.type == 'active':
+        #     metrics, cm = self._active_body()
+        #     print(cm)
+
+        # save results
+        del results['config']['trainer']['logger']
+        del results['config']['trainer']['callbacks']
+        with open(self.savepath, 'w') as f:
+            json.dump(results, f, indent=4)
 
 
 if __name__ == "__main__":
